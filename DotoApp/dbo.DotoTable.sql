bd0fac6676c8f01fa61761e1e03bca97da359e1c
S1ms <shuisko@gmail.com>
2019-08-19 13:04:36 +0300

    poistettu kansio

--- DotoApp/dbo.DotoTable.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[DotoTable] (
-    [Id]   INT NOT NULL IDENTITY, 
-    [Hero] NVARCHAR (50) NOT NULL,
-    [WL]   NCHAR (10)    NOT NULL, 
-    CONSTRAINT [PK_DotoTable] PRIMARY KEY ([Id])
-);
